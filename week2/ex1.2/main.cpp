4cfe7e21e2991b15c4ef5706a702a1d599b5e411
kmoilane <karri.moilanen@gmail.com>
2023-05-15 16:09:44 +0300

    Exercise 1.2, takes any two numbers

---
+++ week2/ex1.2/main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+int sum(double a, double b)
+{
+    std::cout << "Sum of " << a << " and " << b << " is; " << a + b << '\n';
+
+    return 0;
+}
+
+// Calculates the difference as an absolute value
+int difference(double a, double b)
+{
+    double diff { };
+
+    diff = a > b ? a - b : b - a;
+    std::cout << "Difference between " << a << " and " << b << " is: " << diff
+        << '\n';
+
+    return 0;
+}
+
+int main()
+{
+    double a { };
+    double b { };
+
+    std::cout << "Enter the first number (any number):\n";
+    std::cin >> a;
+
+    std::cout << "Enter the second number (any number):\n";
+    std::cin >> b;
+
+    sum(a, b);
+    difference(a, b);
+    return 0;
+}
