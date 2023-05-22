204d4ebce5c5f9c9a5b329c47323343a8a1d296a
kmoilane <karri.moilanen@gmail.com>
2023-05-22 16:58:45 +0300

    added comments and function declarartions

--- week3/ex2.2/main.h
+++ week3/ex2.2/main.h
@@ -1,9 +1,21 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-    // Hinnat: Forssa, Neste Oil Express, CM Tammenkuja 2. PVM: 16.05
-    const double gas { 1.899 };
-    const double diesel { 1.714 };
-    const double electricity { 0.0225 }; // 2.25c/kWh 22.05.2023 kello 16:00
+    /*
+    **  Const variables
+    */
+    const double gas { 1.899 };             // €/liter
+    const double diesel { 1.714 };          // €/liter
+    const double electricity { 0.0725 };    // €/kWh (7.25c/kwH)
+
+    /*
+    **  Function Declarations
+    */
+    double getDistance();
+    double getConsumption();
+    double calcTotalConsumption(double distance, double consumption);
+    double calcCost(double totalConsumption, double price);
+
+    int getFuelType();
 
 #endif
